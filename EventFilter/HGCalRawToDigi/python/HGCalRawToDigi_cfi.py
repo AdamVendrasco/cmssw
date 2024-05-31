34cb0e518e43bf40ce69dc083069c371ae8cb0f5
Adam Vendrasco <avendras@vols.utk.edu>
2024-05-31 11:00:05 +0200

    test commit

--- EventFilter/HGCalRawToDigi/python/HGCalRawToDigi_cfi.py
+++ EventFilter/HGCalRawToDigi/python/HGCalRawToDigi_cfi.py
@@ -1,6 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 import EventFilter.HGCalRawToDigi.HGCalRawToDigiFake_cfi
 
+process = cms.Process("RAWTODIGI")
+
+#Just an open data cms root file
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'root://eospublic.cern.ch//eos/opendata/cms/mc/RunIISummer20UL16MiniAODv2/ZZ_TuneCP5_13TeV-pythia8/MINIAODSIM/106X_mcRun2_asymptotic_v17-v1/120000/BB3EB5DF-6B33-E44E-8F0F-646EF7A48DC1.root'
+    )
+)
+
 hgcalDigis = EventFilter.HGCalRawToDigi.HGCalRawToDigiFake_cfi.HGCalRawToDigiFake.clone()
 
 import EventFilter.HGCalRawToDigi.HFNoseRawToDigiFake_cfi
